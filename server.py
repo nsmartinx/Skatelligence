b17d4125d9ca3f4a9388161106d7376685e2a432
nsmartinx <nsmartinx@outlook.com>
2024-07-16 21:59:50 -0400

    Removed directory clearing functionality from the server (moved to graph app). Initial commit for graphing app, just displayes the sensor readings.

--- server.py
+++ server.py
@@ -1,26 +1,11 @@
 from flask import Flask, request
 import os
-import shutil
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "</Path/to/folder>"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-def clear_directory(path):
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
-
-# Clear the upload folder at startup
-clear_directory(UPLOAD_FOLDER)
 
 @app.route('/postdata', methods=['POST'])
 def upload_file():
