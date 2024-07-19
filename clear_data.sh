4d74ee44ee9ce342803803786499cda1a0bdd4c4
nsmartinx <nsmartinx@outlook.com>
2024-07-19 13:50:16 -0400

    made script create directory data if it doesnt exist

--- clear_data.sh
+++ clear_data.sh
@@ -1,1 +1,13 @@
-rm data/*
+#!/bin/bash
+
+# Check if the directory 'data' exists in the current directory
+if [ -d "data" ]; then
+    # If the directory exists, clear its contents
+    rm -rf data/*
+    echo "Directory 'data' exists. Contents cleared."
+else
+    # If the directory does not exist, create it
+    mkdir data
+    echo "Directory 'data' created."
+fi
+
