fb728a17714b1d80bb3c55d58e7ddb3ba8443e92
nsmartinx <nsmartinx@outlook.com>
2024-07-20 10:59:30 -0400

    made clearing jump direcotyr optional

--- clear_data.sh
+++ clear_data.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-# Check if the directory 'data' exists in the current directory
-if [ -d "data" ]; then
-    # If the directory exists, clear its contents
-    rm -rf data/*
-    echo "Directory 'data' exists. Contents cleared."
-else
-    # If the directory does not exist, create it
-    mkdir data
-    echo "Directory 'data' created."
-fi
+# Function to clear directory
+clear_directory() {
+    local dir=$1
+    if [ -d "$dir" ]; then
+        rm -rf ${dir}/*
+        echo "Directory '$dir' exists. Contents cleared."
+    else
+        mkdir $dir
+        echo "Directory '$dir' created."
+    fi
+}
 
-# Check if the directory 'processed_data' exists in the current directory
-if [ -d "processed_data" ]; then
-    # If the directory exists, clear its contents
-    rm -rf processed_data/*
-    echo "Directory 'processed_data' exists. Contents cleared."
-else
-    # If the directory does not exist, create it
-    mkdir processed_data
-    echo "Directory 'processed_data' created."
-fi
+# List of directories to always clear
+directories=("data" "processed_data")
 
-# Check if the directory 'processed_data' exists in the current directory
-if [ -d "jumps" ]; then
-    # If the directory exists, clear its contents
-    rm -rf jumps/*
-    echo "Directory 'jumps' exists. Contents cleared."
-else
-    # If the directory does not exist, create it
-    mkdir jumps
-    echo "Directory 'jumps' created."
+# Clear the specified directories
+for dir in ${directories[@]}; do
+    clear_directory $dir
+done
+
+# Check if 'j' argument is specified to clear 'jumps'
+if [[ "$1" == "j" ]]; then
+    clear_directory "jumps"
 fi
 
