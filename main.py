dca56f76ed40279d706a5f78c52b6cea0d82ecba
nsmartinx <nsmartinx@outlook.com>
2024-07-22 22:02:51 -0400

    added comment

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from graph import MainApplication
-
+'''
+Starts the application, most other functionalty should be implemented in other files
+'''
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_app = MainApplication()
