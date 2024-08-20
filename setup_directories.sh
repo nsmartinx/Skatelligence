e40daa26317e6bb3a68b26914dc99da1d3f9770b
Nathan Martin <nsmartinx@outlook.com>
2024-08-20 14:14:05 -0400

    GAN created

--- setup_directories.sh
+++ setup_directories.sh
@@ -2,11 +2,14 @@
 
 # Base directories
 mkdir -p data
-mkdir -p data/{labeled_data,recordings,live}
+mkdir -p data/{labeled_data,recordings,live,generated_data}
 mkdir -p data/live/{raw_data,jumps,processed_data}
 
 # Subdirectories within labeled_data
 cd data/labeled_data
+mkdir -p {axel,flip,loop,lutz,salchow,toe}
+cd ../generated_data
+mkdir -p {axel,flip,loop,lutz,salchow,toe}
 
 echo "Directory structure has been set up."
 
