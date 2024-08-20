56be9b7c49da0d0bd3b7034c1686261940320032
nsmartinx <nsmartinx@outlook.com>
2024-08-19 21:37:07 -0400

    removed directories for different spins of jumps

--- setup_directories.sh
+++ setup_directories.sh
@@ -7,12 +7,6 @@ mkdir -p data/live/{raw_data,jumps,processed_data}
 
 # Subdirectories within labeled_data
 cd data/labeled_data
-mkdir -p axel/{double,quad,single,triple}
-mkdir -p flip/{double,quad,single,triple}
-mkdir -p loop/{double,quad,single,triple}
-mkdir -p lutz/{double,quad,single,triple}
-mkdir -p salchow/{double,quad,single,triple}
-mkdir -p toe/{double,quad,single,triple}
 
 echo "Directory structure has been set up."
 
